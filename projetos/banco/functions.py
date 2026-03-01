418b7b833da47d50d3a5b0a805c4648fd2be0fb1
gabrielnextjs <gabriel.9824592@aluno.mg.gov.br>
2026-02-28 21:20:56 -0300

    add account management functions and main menu for banking application

---
+++ projetos/banco/functions.py
@@ -0,0 +1,109 @@
+import json
+import hashlib
+
+def carregar_contas():
+    with open("/home/gabriel/Documentos/Projeto/banco/projetos/banco/contas.json" , "r") as arquivo:
+        contas = json.load(arquivo)
+    return contas
+
+def salvar_contas(contas):
+    with open("/home/gabriel/Documentos/Projeto/banco/projetos/banco/contas.json" , "w") as arquivo:
+        json.dump(contas, arquivo)
+
+def cadastrar(contas):
+    nome = input("Insira o nome de usuario da conta: ")
+    while True:
+        try:
+            senha = hashear(int(input("Digite a senha da conta: ")))
+            agencia = int(input("Digite a agencia da conta: "))
+            break
+        except ValueError:
+            print("Digite apenas numeros")
+    saldo = 0
+
+    numero = str(len(contas) + 1).zfill(4)
+
+    contas[numero] = {
+        "nome": nome,
+        "senha": senha,
+        "agencia": agencia,
+        "saldo": saldo,
+    }
+    salvar_contas(contas)
+
+def login(contas):
+    while True:
+        try:
+            numero = input("Digite o numero da conta: ").zfill(4)
+            senha = int(input("Digite a senha da conta: "))
+            break
+        except ValueError:
+            print("Digite apenas numeros")
+
+    if numero in contas:
+        if contas[numero]["senha"] == hashear(senha):
+            print("Login Realizado")
+            contas[numero]["numero"] = numero.zfill(4)
+            return contas[numero]
+        print("Senha incorreta")
+    else:
+        print("Conta nao encontrada")
+
+    return None
+
+def depositar(conta_logada, contas):
+    while True:
+        try:
+            valor = int(input("Digite o  valor do deposito: "))
+            break
+        except ValueError:
+            print("Digite apenas numeros")
+
+    if valor == 0:
+        print("O valor deve ser maior doque zero absoluto")
+    else:
+        deseja = 0
+        while deseja != 2:
+            deseja = int(input(f'''
+Voce esta depositando para conta: {contas[conta_logada["numero"]]["nome"] } digite [1] para prosseguir e [2] para cancelar: '''))
+            if deseja == 1:
+                contas[conta_logada["numero"]]["saldo"] += valor
+                salvar_contas(contas)
+                print(f"deposito de valor R$ {valor:.2f} realizado")
+                break
+            elif deseja == 2:
+                break
+
+def trasferir(conta_logada, contas):
+        while True:
+            try:
+                destino = input("Digite o numero da conta pra qual deseja trasferir: ").zfill(4)
+                valor = int(input("Digite o  valor da transferencia: "))
+                break
+            except ValueError:
+                print("Digite apenas numeros")
+
+        if destino not in contas:
+            print("Esta conta nao existe")
+        elif valor == 0:
+            print("O valor deve ser maior doque zero absoluto")
+        elif destino == conta_logada["numero"]:
+            print("Voce nao pode trasferir para voce mesmo")
+        elif conta_logada["saldo"] < valor:
+            print("Saldo insuficiente")
+        else:
+            deseja = 0
+            while deseja != 2:
+                deseja = int(input(f'''
+Voce esta trasferindo para conta: {contas[destino]["nome"]} digite [1] para prosseguir e [2] para cancelar: '''))
+                if deseja == 1:
+                    contas[conta_logada["numero"]]['saldo'] -= valor
+                    contas[destino]["saldo"] += valor
+                    salvar_contas(contas)
+                    print(f"Trasferencia de valor R$ {valor:.2f} realizada")
+                    break
+                elif deseja == 2:
+                    break
+
+def hashear(senha):
+    return hashlib.sha256(str(senha).encode()).hexdigest()
---
+++ projetos/banco/main.py
@@ -0,0 +1,52 @@
+from functions import *
+contas  = carregar_contas()
+
+print("BEM VINDO!")
+
+menu = 0
+
+while menu != 3:
+    while True:
+        try:
+            menu = int(input('''
+[1]Criar conta
+[2]Login na conta
+[3]Sair
+INSIRA: '''))
+            break
+        except ValueError:
+            print("Digite apenas numeros")
+
+
+    if menu == 3:
+        print("Voce saiu")
+        break
+    if menu == 1:
+        cadastrar(contas)
+    elif menu  == 2:
+        conta_logada = login(contas)
+
+        if conta_logada:
+                menu_conta = 0
+                while menu_conta != 4:
+                    while True:
+                        try:
+                            menu_conta = int(input('''
+[1]Ver saldo
+[2]Transferir
+[3]Depositar
+[4]Sair
+INSIRA: '''))
+                            break
+                        except ValueError:
+                            print("Digite apenas numeros")
+
+                    if menu_conta == 1:
+                        print(f"Saldo: R$ {conta_logada['saldo']:.2f}")
+                    elif menu_conta == 2:
+                        trasferir(conta_logada, contas)
+                    elif menu_conta == 3:
+                        depositar(conta_logada, contas)
+                    elif menu_conta == 4:
+                        print("Voce saiu!")
+                        break
